08340ada8f5be5b0f0ba883ee9de9cc5964528be
Elizabeth <elizabeththomas.mec@gmail.com>
2022-09-08 23:19:18 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
+import React from 'react'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps as NextAppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// modified version - allows for custom pageProps type, falling back to 'any'
+type AppProps<P = any> = {
+  pageProps: P
+} & Omit<NextAppProps<P>, 'pageProps'>
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  // To make sure data is not shared between users and request
+  const queryClient = React.useRef(new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
